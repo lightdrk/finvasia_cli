b735833aa135982f8278e9af055ca9cc6eef8cf1
lightdrk <108566237+lightdrk@users.noreply.github.com>
2023-07-24 14:50:23 +0530

    Add files via upload

--- finvasia_cli/helper/orderBook.py
+++ finvasia_cli/helper/orderBook.py
@@ -6,16 +6,14 @@ def orderBook(data,api):
     #data = no need
     if data == None:
         response_orderBook = api.get_order_book()
-        for num,n in enumerate(response_orderBook):
-            print(Fore.RED+f'{num}',Fore.GREEN + f'{n}')
-            print()
     else:
         response_orderBook = api.single_order_history(data)
-        try:
-            if response_orderBook['emsg']:
-                print(response_orderBook)
-                exit(Fore.RED+f'{response_orderBook["emsg"]}')
-        except KeyError:
-            if (response_orderBook['stat']).lower() == 'ok':
-                exit(Fore.GREEN+f'{response_orderBook}')
+        print(response_orderBook)
+
+    try:
+        if response_orderBook['emsg']:
+            exit(Fore.RED+f'{response_orderBook["emsg"]}')
+    except KeyError:
+        if (response_orderBook['stat']).lower() == 'ok':
+            exit(Fore.GREEN + f'============Order Caneled =============')
     return response_orderBook
